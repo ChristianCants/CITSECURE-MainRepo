f74da89de35bddbd076379233de080f2ccdac1a4
Jaimes Edward J. Cabante <jaimesedward.cabante@cit.edu>
2023-12-06 17:12:16 +0800

    extra

--- src/components/AcademicBuilding.js
+++ src/components/AcademicBuilding.js
@@ -5,7 +5,7 @@ const AcademicBuilding = () => {
   return (
     <div>
       <h2>Academic Building</h2>
-      <p>This is the Academic Building component.</p>
+      <p>This is the Academic Building componedawdwadwadadwdadwant.</p>
       {/* Add more content specific to Academic Building */}
     </div>
   );
