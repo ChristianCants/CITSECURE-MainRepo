9629cdf6688b102fb4aadd76fd8d606c59ed9207
Jaimes Edward J. Cabante <jaimesedward.cabante@cit.edu>
2023-12-13 19:28:41 +0800

    Adding rooms to Storey Buildings

--- src/components/AlliedBuilding.js
+++ src/components/AlliedBuilding.js
@@ -1,57 +1,298 @@
-// AlliedBuilding.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-const AlliedBuilding = () => {
-  const containerStyle = {
-    minHeight: '100vh',
+const GLEBuilding = () => {
+  const [activeLink, setActiveLink] = useState('view-map');
+  const navigate = useNavigate();
+
+  const pageStyles = {
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    background: '#FFFFFF',
   };
 
-  const buttonStyle = {
-    width: '400px',
-    height: '70px',
-    flexShrink: 0,
-    borderRadius: '100px',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+  const titleStyles = {
+    marginTop: '20px',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+  };
+
+  const cardStyles = {
+    marginTop: '20px',
+    marginBottom: '10px'
+  };
+
+  const cardWrapperStyles = {
+    marginBottom: '10px', // Set the bottom margin for consistent spacing
+  };
+
+  const navBarStyles = {
+    backgroundColor: 'maroon',
+    width: '100%',
+    padding: '10px',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    fontSize: '30px',
-    marginTop: '50px',
+    justifyContent: 'space-between',
   };
 
-  const addRoomButtonStyle = {
-    ...buttonStyle,
-    background: '#FFF9EB',
-    border: '2px solid black',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.5), 0 8px 16px rgba(0, 0, 0, 0.91)',
+  const navLinkStyles = {
+    color: 'white',
+    margin: '0 80px',
+    position: 'relative',
   };
 
-  const viewRoomButtonStyle = {
-    ...buttonStyle,
-    background: '#FFF9EB',
-    border: '2px solid black',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.5), 0 8px 16px rgba(0, 0, 0, 0.91)',
-  };
+  const navLinks = [
+    { label: 'Home', href: '/menu', id: 'home' },
+    { label: 'View Map', href: '/view-map', id: 'view-map' },
+    { label: 'Building', href: '/building', id: 'building' },
+    // Add more navigation links as needed
+  ];
 
   return (
-    <div style={containerStyle}>
-      <Link to="/add-room" className="btn btn-primary btn-lg rounded-pill mx-2" style={addRoomButtonStyle}>
-        <span style={{ marginRight: '8px' }}>Add Room</span>
-        <svg className="bi" width="24" height="24">
-          <use xlinkHref="#plus-circle"></use>
-        </svg>
-      </Link>
-      <Link to="/view-room" className="btn btn-outline-secondary btn-lg rounded-pill mx-2" style={viewRoomButtonStyle}>
-        <span style={{ marginRight: '8px' }}>View Room</span>
-      </Link>
+    <div style={pageStyles}>
+      {/* Navigation Bar */}
+      <div style={navBarStyles}>
+        <div
+          style={{
+            color: 'white',
+            marginLeft: '10px',
+            cursor: 'pointer',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+          onClick={() => {
+            setActiveLink('view-map');
+            navigate('/view-map');
+          }}
+        >
+          {/* Your logo or icon */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 56 54" fill="none">
+            <path
+              d="M2.91855 24.6698L53.7146 2.74497L28.2999 51.8879L23.7747 30.6645L2.91855 24.6698Z"
+              stroke="white"
+              strokeWidth="4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span style={{ width: '1px', height: '35px', backgroundColor: 'white', margin: '0 5px' }}></span>
+          <span>CIT NaviGO</span>
+        </div>
+        <div>
+          {navLinks.map((link) => (
+            <Link
+              key={link.id}
+              to={link.href}
+              style={{
+                ...navLinkStyles,
+                color: activeLink === link.id ? 'white' : 'white',
+              }}
+              onClick={() => setActiveLink(link.id)}
+            >
+              {link.label}
+              {link.id === 'view-map' && (
+                <div
+                  style={{
+                    content: '""',
+                    height: '2px',
+                    width: '100%',
+                    backgroundColor: 'white',
+                    position: 'absolute',
+                    bottom: '-4px',
+                    left: 0,
+                    display: activeLink === link.id ? 'block' : 'none',
+                    transition: '0.3s',
+                  }}
+                ></div>
+              )}
+            </Link>
+          ))}
+        </div>
+
+        {/* Dropdown Button */}
+        <Dropdown>
+          <Dropdown.Toggle variant="link" id="dropdown-basic" style={{ color: 'white', marginLeft: '10px' }}>
+            <img src="/images/Avatar.png" alt="Your Image" width="32" height="32" className="rounded-circle" />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => console.log('Profile clicked')}>Profile</Dropdown.Item>
+            <Dropdown.Item onClick={() => console.log('Logout clicked')}>Logout</Dropdown.Item>
+            {/* Add more dropdown items as needed */}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
+      {/* Title */}
+      <div style={titleStyles}>ALLIED BUILDING ROOMS</div>
+
+      {/* Cards Section */}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3" style={{ ...cardStyles, ...cardWrapperStyles }}>
+            {/* Card 1 */}
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Primary card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Add more cards with the same structure */}
+          {/* Card 2 */}
+          <div className="col-md-3" style={{ ...cardStyles }}>
+          <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Secondary card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 3 */}
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Success card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 4 */}
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Danger card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 5 */}
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Warning card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 6 */}
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Info card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 7 */}
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Light card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 8 */}
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-3" style={{ ...cardStyles }}>
+            <div className="card text-black" style={{ backgroundColor: '#FFEBCD', ...cardStyles }}>
+              <div className="card-header">Room Id</div>
+              <div className="card-body">
+                <h5 className="card-title">Dark card title</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Add more cards with the same structure */}
+        </div>
+      </div>
+
+      {/* Your View Map content */}
+      {/* ... */}
     </div>
   );
 };
 
-export default AlliedBuilding;
+export default GLEBuilding;
