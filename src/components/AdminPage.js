8a9b298df8a480d6f87d6a009891f4110d200935
ChristianCants <christiancantiveros660@gmail.com>
2023-12-20 23:22:16 +0800

    adding update and delete function

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Nav, Dropdown, Table, Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, Dropdown, Table, Container, Row, Col, Button, Modal, Form } from 'react-bootstrap';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';  // Add this line to import useNavigate
+import { useNavigate } from 'react-router-dom';
 
 const AdminPage = () => {
   const [users, setUsers] = useState([]);
-  const navigate = useNavigate();  // Add this line to use navigate
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [updatedUserData, setUpdatedUserData] = useState({
+    firstName: '',
+    lastName: '',
+  });
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch users from the backend when the component mounts
@@ -19,7 +26,7 @@ const AdminPage = () => {
   }, []);
 
   const navBarStyles = {
-    backgroundColor: '#DC8686', // Change the background color to #B06161
+    backgroundColor: '#B06161',
     display: 'flex',
     alignItems: 'center',
     padding: '10px',
@@ -35,6 +42,66 @@ const AdminPage = () => {
     alignItems: 'center',
   };
 
+  const handleUpdate = (userId) => {
+    setSelectedUserId(userId);
+    setShowUpdateModal(true);
+  };
+
+  const handleUpdateModalClose = () => {
+    setShowUpdateModal(false);
+    setSelectedUserId(null);
+    setUpdatedUserData({
+      firstName: '',
+      lastName: '',
+    });
+  };
+
+  const handleDelete = async (userId) => {
+    try {
+      const confirmDelete = window.confirm('Are you sure you want to delete this user?');
+      if (confirmDelete) {
+        const response = await axios.delete(`http://localhost:8080/User/deleteUser/${userId}`);
+        if (response.status === 200) {
+          alert('User deleted successfully!');
+          // You may want to fetch the updated user list after deletion
+          // to reflect the changes in the table.
+          // For simplicity, you can re-fetch the user list here.
+          const updatedUsers = await axios.get('http://localhost:8080/User/getAllUsers');
+          setUsers(updatedUsers.data);
+        } else {
+          alert('Failed to delete user.');
+        }
+      }
+    } catch (error) {
+      console.error('Error deleting user:', error.message);
+    }
+  };
+  
+
+  const handleUpdateModalSave = async () => {
+    try {
+      const response = await axios.put(`http://localhost:8080/User/updateUser/${selectedUserId}`, {
+        firstName: updatedUserData.firstName,
+        lastName: updatedUserData.lastName,
+      });
+  
+      if (response.status === 200) {
+        alert('User updated successfully!');
+        // Fetch the updated user list after the update to reflect changes in the table
+        const updatedUsers = await axios.get('http://localhost:8080/User/getAllUsers');
+        setUsers(updatedUsers.data);
+      } else {
+        alert('Failed to update user.');
+      }
+  
+      // Close the modal after updating
+      handleUpdateModalClose();
+    } catch (error) {
+      console.error('Error updating user:', error.message);
+    }
+  };
+  
+
   return (
     <>
       <Navbar className="d-flex flex-wrap align-items-center justify-content-between py-3 mb-4 border-bottom" style={navBarStyles}>
@@ -44,65 +111,91 @@ const AdminPage = () => {
         <Nav className="d-flex align-items-center">
           {/* Add your navigation links here */}
           {/* ... */}
-
           {/* Dropdown Button */}
           <Dropdown>
             {/* Dropdown content */}
           </Dropdown>
-
           {/* Logout button */}
         </Nav>
       </Navbar>
 
       <Container fluid className="py-5">
-      <Row>
-  <Col>
-    {/* Breadcrumb navigation */}
-    <nav aria-label="breadcrumb" className="rounded-3 p-3 mb-4" style={{ backgroundColor: 'rgb(164, 63, 63)' }}>
-      <ol className="breadcrumb mb-0" style={{ backgroundColor: 'rgb(164, 63, 63)', color: 'white' }}>
-        <li className="breadcrumb-item" onClick={() => navigate('/menu')}>
-          <a href="#" style={{ color: 'white' }}>Admin</a>
-        </li>
-        <li className="breadcrumb-item">
-          <a href="#" style={{ color: 'white' }}>User</a>
-        </li>
-        <li className="breadcrumb-item active" aria-current="page">
-          User Profile
-        </li>
-      </ol>
-    </nav>
-  </Col>
-</Row>
+        <Row>
+          <Col>
+            {/* ... (previous code) */}
+          </Col>
+        </Row>
 
         <Row>
           <Col lg={12}>
-          
-          <Table striped bordered hover style={{ border: '1px solid #B06161', color: 'black' }}>
-  <thead>
-    <tr>
-      <th style={{ verticalAlign: 'bottom', borderBottom: '2px solid #dc3545' }}>ID</th>
-      <th style={{ verticalAlign: 'bottom', borderBottom: '2px solid #dc3545' }}>First Name</th>
-      <th style={{ verticalAlign: 'bottom', borderBottom: '2px solid #dc3545' }}>Last Name</th>
-      <th style={{ verticalAlign: 'bottom', borderBottom: '2px solid #dc3545' }}>Gender</th>
-      <th style={{ verticalAlign: 'bottom', borderBottom: '2px solid #dc3545' }}>Email</th>
-    </tr>
-  </thead>
-  <tbody style={{ color: 'black' }}>
-  {users.map((user) => (
-    <tr key={user.id}>
-      <td style={{ borderBottom: '1px solid #B06161' }}>{user.id}</td>
-      <td style={{ borderBottom: '1px solid #B06161' }}>{user.firstName}</td>
-      <td style={{ borderBottom: '1px solid #B06161' }}>{user.lastName}</td>
-      <td style={{ borderBottom: '1px solid #B06161' }}>{user.gender}</td>
-      <td style={{ borderBottom: '1px solid #B06161' }}>{user.email}</td>
-    </tr>
-  ))}
-</tbody>
-</Table>
-
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Gender</th>
+                  <th>Email</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody style={{ color: 'black' }}>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td style={{ borderBottom: '1px solid #B06161' }}>{user.id}</td>
+                    <td style={{ borderBottom: '1px solid #B06161' }}>{user.firstName}</td>
+                    <td style={{ borderBottom: '1px solid #B06161' }}>{user.lastName}</td>
+                    <td style={{ borderBottom: '1px solid #B06161' }}>{user.gender}</td>
+                    <td style={{ borderBottom: '1px solid #B06161' }}>{user.email}</td>
+                    <td style={{ borderBottom: '1px solid #B06161' }}>
+                      <Button variant="info" onClick={() => handleUpdate(user.id)}>Update</Button>
+                      {/* Use a different onClick handler for the delete button */}
+                      <Button variant="danger" onClick={() => handleDelete(user.id)}>Delete</Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
           </Col>
         </Row>
       </Container>
+
+      {/* Update Modal */}
+      <Modal show={showUpdateModal} onHide={handleUpdateModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter first name"
+                value={updatedUserData.firstName}
+                onChange={(e) => setUpdatedUserData({ ...updatedUserData, firstName: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter last name"
+                value={updatedUserData.lastName}
+                onChange={(e) => setUpdatedUserData({ ...updatedUserData, lastName: e.target.value })}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleUpdateModalClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleUpdateModalSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
