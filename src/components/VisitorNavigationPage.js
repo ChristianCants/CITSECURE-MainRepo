e06e59506e372166c69dd9e9634f619108d3916c
ChristianCants <christiancantiveros660@gmail.com>
2024-04-13 02:43:21 +0800

    HAHAHAHAHAHHAHAH

--- src/components/VisitorNavigationPage.js
+++ src/components/VisitorNavigationPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VisitorNavigationPage = () => {
     return (
         <div>
-            <h1>Visitor Navigation</h1>
+            <h1>Joke rani VisitorNavigationPage kay wala pako idea unsa ibutang ani. ako rani gituyo kay aron dali ra ma butang puhon ang sakto na sud ani thanks. -Cloud</h1>
             {/* Add navigation information and links here */}
         </div>
     );
