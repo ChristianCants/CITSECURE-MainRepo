e51ec02c80b448b84ed5db0ab7b1e49ffc44e2a1
ChristianCants <christiancantiveros660@gmail.com>
2024-05-01 02:37:03 +0800

    Clearing some unused variables

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Button, Alert } from 'react-bootstrap';
+import { Modal, Button, } from 'react-bootstrap';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Chip from '@mui/material/Chip';
