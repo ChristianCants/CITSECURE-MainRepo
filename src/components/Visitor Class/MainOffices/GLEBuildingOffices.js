349168d9883c090fbf5443b9ea9a3831e02154c6
Jaimes Edward J. Cabante <jaimesedward.cabante@cit.edu>
2024-11-05 23:47:14 +0800

    2 Acad and GLE Office Added

--- src/components/Visitor Class/MainOffices/GLEBuildingOffices.js
+++ src/components/Visitor Class/MainOffices/GLEBuildingOffices.js
@@ -80,14 +80,14 @@ class GLEBuildingOffices extends React.Component {
                         <Carousel.Item>
                             <img
                                 className="d-block w-100"
-                                src="images/Academic Building.jpg"
-                                alt="Academic Building"
-                                style={{ height: '700px', width: '100%', objectFit: 'cover' }}
+                                src="images/GLE Exhibit Area.jpg"
+                                alt="Exhibit Area"
+                                style={{ height: '800px', width: '100%', objectFit: 'cover' }}
                             />
                             <Carousel.Caption style={carouselCaptionStyle}>
                                 <div style={{ color: 'black', display: 'flex', flexDirection: 'column' }}>
-                                    <h5>Academic Building</h5>
-                                    <p>The Academic Building is a hub for various academic and administrative functions, including Architecture, Tourism, Industrial Engineering, Hospital Management, Alumni Office, Safety and Security, and Property Custodian Office.</p>
+                                    <h5>Exhibit Area</h5>
+                                    <p>Need Details</p>
                                 </div>
                             </Carousel.Caption>
                         </Carousel.Item>
@@ -96,14 +96,14 @@ class GLEBuildingOffices extends React.Component {
                         <Carousel.Item>
                             <img
                                 className="d-block w-100"
-                                src="images/High School Building.jpg"
-                                alt="High School Building"
-                                style={{ height: '700px', width: '100%', objectFit: 'cover' }}
+                                src="images/GLE Seminar Room.jpg"
+                                alt="GLE Seminar Area"
+                                style={{ height: '800px', width: '100%', objectFit: 'cover' }}
                             />
                             <Carousel.Caption style={carouselCaptionStyle}>
                                 <div style={{ color: 'black', display: 'flex', flexDirection: 'column' }}>
-                                    <h5>High School Building</h5>
-                                    <p>Explore the hallways of our high school, a dynamic environment geared for academic advancement and exciting student life.</p>
+                                    <h5>Seminar Room</h5>
+                                    <p>Need Details</p>
                                 </div>
                             </Carousel.Caption>
                         </Carousel.Item>
